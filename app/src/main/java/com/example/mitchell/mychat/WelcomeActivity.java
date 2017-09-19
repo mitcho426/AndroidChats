afae9559a76e43e25ad226833c6e2238d1848b6d
Mwong <mwong153@gmail.com>
2017-09-19 01:36:35 -0700

    Added a lot

--- app/src/main/java/com/example/mitchell/mychat/WelcomeActivity.java
+++ app/src/main/java/com/example/mitchell/mychat/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.mitchell.mychat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,31 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    //Display this screen for 3 seconds
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+                    //Send user to Login Activity
+                    Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+
+        thread.start();
     }
+
+    private int count = 5;
+
+
+
+
 }
