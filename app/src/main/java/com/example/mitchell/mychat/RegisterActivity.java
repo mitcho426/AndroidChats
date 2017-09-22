64804e558063e6600fbecd3613fa06ef959acece
Mwong <mwong153@gmail.com>
2017-09-22 01:46:47 -0700

    Registering now saves users into Firebase database

--- app/src/main/java/com/example/mitchell/mychat/RegisterActivity.java
+++ app/src/main/java/com/example/mitchell/mychat/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.mitchell.mychat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -15,11 +16,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.R.attr.name;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
     private Toolbar mToolBar;
+    private ProgressDialog loadingBar;
 
     private EditText registerName;
     private EditText registerEmail;
@@ -43,6 +50,8 @@ public class RegisterActivity extends AppCompatActivity {
         registerEmail = (EditText) findViewById(R.id.emailText);
         registerPassword = (EditText) findViewById(R.id.login_password);
 
+        loadingBar = new ProgressDialog(this);
+
         registerButton = (Button) findViewById(R.id.createAccountButton);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +69,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-    private void registerAccount(String name, String email, String password) {
+    private void registerAccount(final String name, final String email, final String password) {
         if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
             Toast.makeText(RegisterActivity.this, "One of your fields are empty. Please try again.", Toast.LENGTH_LONG).show();
         } else {
@@ -68,10 +77,22 @@ public class RegisterActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()) {
-                        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(mainIntent);
-                        finish();
+                        String current_user_id = mAuth.getCurrentUser().getUid();
+                        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(current_user_id);
+
+                        userRef.child("user_name").setValue(name);
+                        userRef.child("user_status").setValue("I am god");
+                        userRef.child("user_image").setValue("default_profile");
+                        userRef.child("user_thumb_image").setValue("default_image")
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(mainIntent);
+                                        finish();
+                                    }
+                                });
                     } else {
                         Toast.makeText(RegisterActivity.this, "Could not create account.", Toast.LENGTH_LONG).show();
                     }
