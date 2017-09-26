cda0cafd737978472fd3fd9ba768ebffaa883161
Mwong <mwong153@gmail.com>
2017-09-26 00:12:58 -0700

    Implemented dataOnChange in Account Settings Activity

--- app/src/main/java/com/example/mitchell/mychat/SettingsActivity.java
+++ app/src/main/java/com/example/mitchell/mychat/SettingsActivity.java
@@ -2,12 +2,61 @@ package com.example.mitchell.mychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private CircleImageView settingsDisplayImage;
+    private TextView settingsDisplayName;
+    private TextView settingsDisplayStatus;
+    private Button settingsChangeProfileImageButton;
+    private Button settingsChangeStatusButton;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mAuth = FirebaseAuth.getInstance();
+        String current_user_id = mAuth.getCurrentUser().getUid();
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(current_user_id);
+
+        settingsDisplayImage = (CircleImageView) findViewById(R.id.settings_profile_image);
+        settingsDisplayName = (TextView) findViewById(R.id.settings_username);
+        settingsDisplayStatus = (TextView) findViewById(R.id.settings_user_status);
+        settingsChangeProfileImageButton = (Button) findViewById(R.id.settings_change_profile_image_button);
+        settingsChangeStatusButton = (Button) findViewById(R.id.settings_change_profile_image_status);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("user_name").getValue().toString();
+                String status = dataSnapshot.child("user_status").getValue().toString();
+                String image = dataSnapshot.child("user_image").getValue().toString();
+                String thumb_image = dataSnapshot.child("user_thumb_image").getValue().toString();
+
+                settingsDisplayName.setText(name);
+                settingsDisplayStatus.setText(status);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
