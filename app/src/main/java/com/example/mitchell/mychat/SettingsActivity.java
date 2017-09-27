7de54d843c75d607c528e7c86f841d02c70947cc
Mwong <mwong153@gmail.com>
2017-09-26 19:38:36 -0700

    Implemented Change profile Image with cocoapods cropping

--- app/src/main/java/com/example/mitchell/mychat/SettingsActivity.java
+++ app/src/main/java/com/example/mitchell/mychat/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.mitchell.mychat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +14,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -21,6 +26,8 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView settingsDisplayStatus;
     private Button settingsChangeProfileImageButton;
     private Button settingsChangeStatusButton;
+
+    private final static int gallery_pick = 1;
 
     private FirebaseAuth mAuth;
     private DatabaseReference userRef;
@@ -58,5 +65,29 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+
+        settingsChangeProfileImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Sends user to mobile phone gallery app
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, gallery_pick);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == gallery_pick && resultCode == RESULT_OK && data != null) {
+
+            Uri imageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this);
+        }
     }
 }
